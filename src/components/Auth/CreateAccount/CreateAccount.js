f5bf49136e8b789d07c73eafc30d9f17b63ccf8b
Prangan Sen <prangan@eatlbd.com>
2024-10-22 19:02:17 +0600

    added changes for authentication

--- src/components/Auth/CreateAccount/CreateAccount.js
+++ src/components/Auth/CreateAccount/CreateAccount.js
@@ -7,7 +7,7 @@ import { AuthUser } from "../../../helpers/AuthUser";
 import { useDispatch, useSelector } from "react-redux";
 import { UrlBuilder } from "../../../helpers/UrlBuilder";
 import { callApi, clearState, selectApi } from "../../../reducers/apiSlice";
-import { ClockIcon, LockClosedIcon } from '@heroicons/react/24/solid'
+import { ClockIcon, LockClosedIcon } from "@heroicons/react/24/solid";
 // import ethUtil from "ethereumjs-util"; // Import ethereumjs-util
 
 const CreateAccount = () => {
@@ -69,23 +69,13 @@ const CreateAccount = () => {
     const address = account.address;
 
     // Derive the public key from the private key
-    const derivedPublicKey = web3.eth.accounts.privateKeyToAccount(privateKey).publicKey; 
+    const derivedPublicKey =
+      web3.eth.accounts.privateKeyToAccount(privateKey).publicKey;
     console.log("Public Key:", derivedPublicKey);
-
-
-
-    // Get public key from private key using ethereumjs-util
-    // const privateKeyBuffer = Buffer.from(privateKey.slice(2), "hex"); // Remove "0x" prefix from privateKey
-    // const publicKeyBuffer = ethUtil.privateToPublic(privateKeyBuffer);
-    // const publicKey = "0x" + publicKeyBuffer.toString("hex");
 
     console.log("Public Key:", derivedPublicKey);
     console.log("Private Key:", privateKey);
     console.log("Public Address:", address);
-
-    // console.log("account", account);
-    // console.log("Private Key:", account.privateKey);
-    // console.log("Public Address:", account.address);
 
     AuthUser.savePublicKey(account.address);
     AuthUser.savePrivateKey(account.privateKey);
@@ -122,46 +112,90 @@ const CreateAccount = () => {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            alt="ARISPAY Wallet"
-            src={ArisPayLogo}
-            className="mx-auto h-28 w-auto"
-          />
-          <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            ARISPAY Crypto Wallet
-          </h2>
-          <p className="mt-6 text-center text-lg font-normal leading-2 tracking-tight text-gray-600">
-            The decentralized web awaits!
-          </p>
-        </div>
+      {AuthUser.getLoggedInUserAddress() &&
+      AuthUser.getLoggedInUserAddress().length > 0 ? (
+        <>
+          <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                alt="ARISPAY Wallet"
+                src={ArisPayLogo}
+                className="mx-auto h-28 w-auto"
+              />
+              <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                ARISPAY Crypto Wallet
+              </h2>
+              <p className="mt-6 text-center text-lg font-normal leading-2 tracking-tight text-gray-600">
+                The decentralized web awaits!
+              </p>
+            </div>
 
-        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white px-6 py-8 shadow-md sm:rounded-lg sm:px-12">
-            <div className="space-y-6">
-
-              <div>
-                <button
-                  onClick={() => handleGenerateKeyPair()}
-                  type="button"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Generate Key Pair
-                </button>
+            <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-[480px]">
+              <div className="bg-white px-6 py-8 shadow-md sm:rounded-lg sm:px-12">
+                <div className="space-y-6">
+                  <div>
+                    <button
+                      onClick={() => handleSendPublicKey()}
+                      type="button"
+                      className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                    >
+                      Go to your wallet account
+                    </button>
+                  </div>
+                </div>
               </div>
-              {AuthUser.getPublicKey() &&
-                AuthUser.getPublicKey().length > 0 && (
-                  <>
-                    <div>
-                      <div
-                        className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 text-center"
-                      >
-                        Public key : <br/> 
-                        {AuthUser.getPublicKey()}
-                      </div>
-                    </div>
-                    {/* <div>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Need help?{" "}
+                <a
+                  href="#"
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                >
+                  Contact ARISPAY Support
+                </a>
+              </p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                alt="ARISPAY Wallet"
+                src={ArisPayLogo}
+                className="mx-auto h-28 w-auto"
+              />
+              <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                ARISPAY Crypto Wallet
+              </h2>
+              <p className="mt-6 text-center text-lg font-normal leading-2 tracking-tight text-gray-600">
+                The decentralized web awaits!
+              </p>
+            </div>
+
+            <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-[480px]">
+              <div className="bg-white px-6 py-8 shadow-md sm:rounded-lg sm:px-12">
+                <div className="space-y-6">
+                  <div>
+                    <button
+                      onClick={() => handleGenerateKeyPair()}
+                      type="button"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Generate Key Pair
+                    </button>
+                  </div>
+                  {AuthUser.getPublicKey() &&
+                    AuthUser.getPublicKey().length > 0 && (
+                      <>
+                        <div>
+                          <div className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 text-center">
+                            Public key : <br />
+                            {AuthUser.getPublicKey()}
+                          </div>
+                        </div>
+                        {/* <div>
                       <button
                         type="button"
                         className="flex w-full justify-center rounded-md bg-green-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 break-all"
@@ -171,54 +205,64 @@ const CreateAccount = () => {
                       </button>
                     </div> */}
 
-<div>
-      <button
-        type="button"
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp} // Clear timer if mouse leaves the button
-        className={`flex w-full justify-center rounded-md ${privateKeyVisible ? "bg-green-500" : "bg-green-400"} px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 break-all`}
-      >
-        {privateKeyVisible ? (
-          <>
-            Private key: <br />
-            {AuthUser.getPrivateKey()}
-          </>
-        ) : (
-          <>
-            <LockClosedIcon className={`mr-2 size-6 ${holding ? "animate-bounce" : ""}`} /> 
-            {/* Optional icon with animation */}
-            Hold the button to reveal the private key
-          </>
-        )}
-      </button>
-    </div>
+                        <div>
+                          <button
+                            type="button"
+                            onMouseDown={handleMouseDown}
+                            onMouseUp={handleMouseUp}
+                            onMouseLeave={handleMouseUp} // Clear timer if mouse leaves the button
+                            className={`flex w-full justify-center rounded-md ${
+                              privateKeyVisible
+                                ? "bg-green-500"
+                                : "bg-green-400"
+                            } px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 break-all`}
+                          >
+                            {privateKeyVisible ? (
+                              <>
+                                Private key: <br />
+                                {AuthUser.getPrivateKey()}
+                              </>
+                            ) : (
+                              <>
+                                <LockClosedIcon
+                                  className={`mr-2 size-6 ${
+                                    holding ? "animate-bounce" : ""
+                                  }`}
+                                />
+                                {/* Optional icon with animation */}
+                                Hold the button to reveal the private key
+                              </>
+                            )}
+                          </button>
+                        </div>
 
-                    <div>
-                      <button
-                        onClick={() => handleSendPublicKey()}
-                        type="button"
-                        className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
-                      >
-                        Go to your wallet account
-                      </button>
-                    </div>
-                  </>
-                )}
+                        <div>
+                          <button
+                            onClick={() => handleSendPublicKey()}
+                            type="button"
+                            className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                          >
+                            Go to your wallet account
+                          </button>
+                        </div>
+                      </>
+                    )}
+                </div>
+              </div>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Need help?{" "}
+                <a
+                  href="#"
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                >
+                  Contact ARISPAY Support
+                </a>
+              </p>
             </div>
           </div>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Need help?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Contact ARISPAY Support
-            </a>
-          </p>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
